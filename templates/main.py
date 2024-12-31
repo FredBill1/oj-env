a9e7f990ae02d2561273e67ef1da2773177afdb9
FredBill1 <36622430+FredBill1@users.noreply.github.com>
2024-12-31 18:13:47 +0000

    update py template

---
+++ templates/main.py
@@ -0,0 +1,14 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+
+
+def solve():
+    N = int(input())
+    a = [int(v) for v in input().split()]
+
+
+for _ in range(int(input())):
+    # solve()
+    print(solve())
+    # print("YES" if solve() else "NO")
